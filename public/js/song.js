59bc3e447406621695db1fd93648510bd60292da
Jeffrey Chen <jeffchen@dhcp-18-30-97-156.dyn.mit.edu>
2019-01-17 10:43:45 -0500

    song.js error
    Former-commit-id: 4f45bcfbf33a56cb99c370c4c2a946165f87e83a

--- public/js/song.js
+++ public/js/song.js
@@ -1,41 +1,42 @@
 function main() {
-    get('/api/whoami', {}, function(user) {
-        console.log(user);
-        var link = {
-            url: 'https://api.spotify.com/v1/tracks/11dFghVXANMlKmJXsNCbNl?market=ES',
-            headers: { 'Authorization': 'Bearer ' + user.access_token },
-            json: true
-          };
-        console.log(link);
-        get(link, {}, function(songData) {
-            renderSongData(songData);
-          });
-      });
+    const profileId = window.location.search.substring(1);
+    console.log("hi")
+    get('/api/user', {'_id': profileId}, function(profileUser) {
+        console.log("fuck")
+      renderSongData(profileUser);
+      console.log(profileUser.name);
+    });
   }
   
-  function renderSongData(user) {
-      
-    // rendering song image
-    const songContainer = document.getElementById('song-image');
-    const songImage = document.createElement('img');
-    songImage.className = "ui medium rounded image";
-    songImage.src = songData.album.images[0].url; // need to fill this out
-    songContainer.appendChild(songImage);
+function renderSongData(profile) {
 
-    //rendering song title
-    const songTitleContainer = document.getElementById('song-title');
-    const songTitle = document.createElement('h1');
-    songTitle.innerHTML = songData.name; //need to fill this out
-    songTitle.className = "ui large header";
-    songTitleContainer.appendChild(songTitle);
+    var artistHeader = [['Authorization', 'Bearer ' + profile.access_token]];
+    get('https://api.spotify.com/v1/tracks/11dFghVXANMlKmJXsNCbNl', {}, function(songData) {
 
-    //rendering song artist
-    const songArtistContainer = document.getElementById('song-artist');
-    const songArtist = document.createElement('p');
-    songArtist.innerHTML = songData.artists[0].name; //need to fill this out
-    songArtist.className = "ui medium header";
-    songArtistContainer.appendChild(songArtist);
-  }
+        console.log(songData.items);
+
+        // rendering song image
+        const songContainer = document.getElementById('song-image');
+        const songImage = document.createElement('img');
+        songImage.className = "ui medium rounded image";
+        songImage.src = songData.album.images[0].url; // need to fill this out
+        songContainer.appendChild(songImage);
+
+        //rendering song title
+        const songTitleContainer = document.getElementById('song-title');
+        const songTitle = document.createElement('h1');
+        songTitle.innerHTML = songData.name; //need to fill this out
+        songTitle.className = "ui large header";
+        songTitleContainer.appendChild(songTitle);
+
+        //rendering song artist
+        const songArtistContainer = document.getElementById('song-artist');
+        const songArtist = document.createElement('p');
+        songArtist.innerHTML = songData.artists[0].name; //need to fill this out
+        songArtist.className = "ui medium header";
+        songArtistContainer.appendChild(songArtist);
+        
+    }, null, artistHeader);
+}
   
   main();
-  
