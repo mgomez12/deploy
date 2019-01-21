d48e3aa2cb6f03b8c4fd621e413067bc71d83f36
Miguel Gomez <mgomez224758@gmail.com>
2019-01-21 04:27:23 -0500

    suggestionform works
    Former-commit-id: 6b92a183decf024691dd8f01a73f151a678da4e3

---
+++ client/src/components/modules/SuggestionForm.js
@@ -0,0 +1,48 @@
+import React, { Component } from 'react';
+import "../../public/css/styles.css"
+import io from 'socket.io-client';
+import { Image, Container, Input } from 'semantic-ui-react';
+import nick_pic from "../../public/assets/nick.jpg";
+import NavBar from "./NavBar";
+import { post } from "./api"
+
+class SuggestionForm extends Component {
+    constructor(props) {
+        super(props);
+
+        this.socket = io('http://localhost:3000');
+
+        this.state = {
+            sender: this.props.userId,
+            receiver: this.props.receiverId,
+            track: ''
+        };
+        this.handleChange = this.handleChange.bind(this)
+        this.submitSuggestion = this.submitSuggestion.bind(this)
+
+    }
+
+    handleChange(event){
+        this.setState({
+            track: event.target.value
+        })
+    }
+
+    submitSuggestion() {
+        console.log('submitted')
+        post('/api/submitSuggestion', {receiver: this.props.receiver._id, sender: this.props.sender._id, track: this.state.track, time: '0'})
+
+    }
+    render() {
+        return(
+            
+<Input
+    action={{ color: 'teal', labelPosition: 'left', content: 'submit', onClick: this.submitSuggestion}}
+    actionPosition='right'
+    placeholder='Search...'
+    onChange={this.handleChange}
+  />
+        )
+    }
+}
+export default SuggestionForm;
