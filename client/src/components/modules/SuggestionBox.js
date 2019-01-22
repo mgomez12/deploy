ca96161fcd4b6818647746aa81b5a6b776b0f63c
Miguel Gomez <mgomez224758@gmail.com>
2019-01-21 23:02:52 -0500

    fixed suggestionbox
    Former-commit-id: ed135169b85cc24590991f824faefb2001726bb3

--- client/src/components/modules/SuggestionBox.js
+++ client/src/components/modules/SuggestionBox.js
@@ -1,28 +1,39 @@
 import React, { Component } from "react";
 import "../../public/css/styles.css";
-import { Button, Header } from "semantic-ui-react";
+import { Button, Header, Card, Feed } from "semantic-ui-react";
 import { Link} from 'react-router-dom';
+import { get } from './api'
 
 class SuggestionBox extends Component {
 	constructor(props) {
         super(props);
 
         this.state = {
+            suggestions:[]
         };
     }
 
     componentDidMount() {
-        
+        let sugg;
+        get('/api/suggestion', {receiver: this.props.user._id, limit:10}, (suggestionArray) => {
+            console.log(suggestionArray)
+            sugg = suggestionArray;
+        })
+        this.setState({
+            suggestions: sugg
+        })
     }
+
 
     render() {
         return (
-            <Card header="Recent suggestions">
-                <Card.item>
-                    <Feed>
-                        
-                    </Feed>
-                </Card.item>
+            <Card>
+                <Card.Header>
+                    Recent Suggestions
+                </Card.Header>
+                <Card.Content style={{overflow:'scroll'}}>
+                    {}
+                </Card.Content>
             </Card>
         )
     }
