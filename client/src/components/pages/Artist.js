7ed8155d8c5a12a551386e6f0e191343595c8cd0
Yu Jing Chen <yujing6@dhcp-18-40-104-120.dyn.mit.edu>
2019-01-29 11:17:13 -0500

    updated stylized artist page

--- client/src/components/pages/Artist.js
+++ client/src/components/pages/Artist.js
@@ -210,29 +210,37 @@ render() {
                         {"Top Songs"}
                     </Header>
                     </section>
-                    <Item.Group>
-                        <Segment.Group>
+                    <Card.Group itemsPerRow={5}>
                     {toptracks.map( track => {
                         const minutes = Math.floor(track.duration_ms / 60000);
                         let seconds = Math.floor(track.duration_ms % 60);
                         if (seconds < 10) {seconds = '0' + seconds}
                         timeString = minutes + ':' + seconds;
                         return(
-                            <Item href={"/song/" + track.id}>
-                            <Segment color="gray">
-                            <Item.Content>
-                                <a href={"/song/" + track.id} id="songdisplay2">{track.name}</a>
-                                <p className="rightaligntext" id="songdisplay2">{timeString}</p>
-                            </Item.Content>
-                            </Segment>
-                            </Item>)
+                            // <Item href={"/song/" + track.id}>
+                            // <Item color="gray">
+                            <Card color="teal"><a href={"/song/" + track.id} id="songdisplay2">
+                            <Card.Content>
+                            <div className="thicker">
+                            <Card.Header className="songheader">
+                                {track.name}
+                            </Card.Header>
+                            <Card.Meta>
+                                <p className="timestamp" id="songdisplay2">{timeString}</p>
+                            </Card.Meta>
+                            </div>
+                            </Card.Content>
+                            </a>
+                            </Card>
+                            // </Item>
+                            // </Item>
+                            )
                     })}
-                    </Segment.Group>
-                    </Item.Group>
+                    </Card.Group>
                     </div>
                     <section>
                     <Header as="h2">
-                                    Full-Length Albums: 
+                                    Full-Length Albums 
                     </Header>
                     </section>
                     <Card.Group itemsPerRow={5}>
@@ -240,7 +248,7 @@ render() {
                     </Card.Group>
                     <section>
                     <Header as="h2">
-                                    Singles and EPs: 
+                                    Singles and EPs 
                     </Header>
                     </section>
                     <Card.Group itemsPerRow={5}>
@@ -248,7 +256,7 @@ render() {
                     </Card.Group>
                     <section>
                     <Header as="h2">
-                                Related Artists: 
+                                Related Artists 
                     </Header>
                     </section>
                     <Coverflow
