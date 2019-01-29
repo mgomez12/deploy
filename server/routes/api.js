cfc7239bff65e45b24d614de05b400bb8177ce93
Miguel Gomez <mgomez224758@gmail.com>
2019-01-29 05:30:06 -0500

    notif

--- server/routes/api.js
+++ server/routes/api.js
@@ -197,10 +197,9 @@ router.post('/friend', function(req, res) {
                     user.friends +=1;
                     user.notifications.push(message)
                     user.save()
-                })
+                })})
                 index = friendObj.sent_request_to.indexOf(req.body.sender);
                 friendObj.sent_request_to.splice(index, 1);
-            })
             }
             else if (!friendObj.received_request_from.includes(req.body.sender)) {
                 console.log('setting true');
